3d8a14e3c0db77e65e7ff81c81e7ae7097aeccd7
kysnez <kysnez312@gmail.com>
2021-12-25 18:59:40 +0300

    add cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from "readline-sync";
+
+export const getUserName = () => {
+  const name = readlineSync.question("Your name: ");
+
+  console.log("Hello, " + name + "!");
+};
+
+//export default getUserName;
