cf60570ba23870e21f621249f83b725d44650473
kysnez <kysnez312@gmail.com>
2022-01-06 22:11:44 +0300

    add src/primeCheck.js

---
+++ src/primeCheck.js
@@ -0,0 +1,18 @@
+export const primeCheck = (number) => {
+  let result = "";
+  const divisors = [];
+
+  if (number === 1) {
+    return (result = "yes");
+  }
+
+  for (let i = 1; i <= number; i += 1) {
+    if (number % i === 0) {
+      divisors.push(i);
+    }
+  }
+
+  result = divisors.length === 2 ? "yes" : "no";
+
+  return result;
+};
