76b78db411030bfed760577f630d23bc78e7ef8e
kysnez <kysnez312@gmail.com>
2022-01-06 18:45:16 +0300

    add getGreatestDivisor

---
+++ src/getGreatestDivisor.js
@@ -0,0 +1,43 @@
+export const getGreatestDivisor = (first, second) => {
+  let result = 0;
+  let firstIndex = 0;
+  let secondIndex = 0;
+
+  if (first === second) {
+    return (result = String(first));
+  }
+
+  const firstDivisors = [];
+  const secondDivisors = [];
+  const commonDivisors = [];
+
+  for (let i = 1; i <= first; i += 1) {
+    if (first % i === 0) {
+      firstDivisors.push(i);
+    }
+  }
+
+  for (let n = 1; n <= second; n += 1) {
+    if (second % n === 0) {
+      secondDivisors.push(n);
+    }
+  }
+
+  const biggestLength =
+    firstDivisors.length > secondDivisors.length
+      ? firstDivisors.length
+      : secondDivisors.length;
+
+  for (let i = 0; i <= biggestLength; i += 1) {
+    if (firstDivisors[firstIndex] === secondDivisors[secondIndex]) {
+      commonDivisors.push(firstDivisors[firstIndex]);
+      firstIndex += 1;
+      secondIndex += 1;
+    } else if (firstDivisors[firstIndex] > secondDivisors[secondIndex]) {
+      secondIndex += 1;
+    } else {
+      firstIndex += 1;
+    }
+  }
+  return commonDivisors[commonDivisors.length - 1];
+};
